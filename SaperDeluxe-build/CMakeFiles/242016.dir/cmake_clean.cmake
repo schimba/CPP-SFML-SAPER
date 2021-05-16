295fe7c19b8ae91595fb5aef22fea6a90907e2c2
schimba <62520800+schimba@users.noreply.github.com>
2021-05-16 22:12:29 +0200

    Added Realease Build

---
+++ SaperDeluxe-build/CMakeFiles/242016.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/242016.dir/main.cpp.obj"
+  "CMakeFiles/242016.dir/Pole.cpp.obj"
+  "CMakeFiles/242016.dir/Plansza.cpp.obj"
+  "242016.pdb"
+  "242016.exe"
+  "242016.exe.manifest"
+  "lib242016.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/242016.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
